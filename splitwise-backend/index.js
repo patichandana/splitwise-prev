b7ddfd4d0473ae6052a0dfe24fd85e52a4a62325
patichandana <iampati.official@gmail.com>
2024-03-25 22:07:52 +0530

    implemented signup and validateotp rest apis

--- splitwise-backend/index.js
+++ splitwise-backend/index.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 // const pg_promise = require("pg-promise")
 const bodyParser = require("body-parser");
 const node_cache = require("node-cache");
-const { randomBytes } = require("crypto");
+const nodeMailer = require("nodemailer");
+require('dotenv').config();
+
+// const { randomBytes } = require("crypto");
+
 const PORT = 3000;
 
 // console.log(a)
@@ -19,6 +23,17 @@ const db = pg_promise({
     password: "password"
 });
 const cache = new node_cache();
+const transportor = nodeMailer.createTransport(
+    {
+        port: 587,
+        host: 'smtp.sendgrid.net',
+        auth: {
+            user: 'apikey',
+            pass: process.env.SENDGRID_API_KEY
+        }
+        // secure: true
+    }
+);
 // console.log(express);
 // console.log(app);
 // console.log(db);
@@ -48,25 +63,25 @@ app.get('/groups/:user_id', (req, res) => {
     counter = 0
     console.log(user_id)
     db.any(`SELECT group_id FROM groupmembers WHERE user_id=${user_id}`)
-    .then(function(data) {
-        // res.send(data);
-        // console.log(data);
-        data.forEach((obj) => {
-            db.any(`SELECT group_name, group_id FROM groups where group_id=${obj.group_id}`).
-                then(function (group) {
-                    console.log(obj, group);
-                    userGroups.push(group[0]);
-                    console.log(userGroups);
-                    console.log(counter, data.length)
-                    ++counter;
-                    if(counter == data.length) {
-                        console.log("entered")
+        .then(function (data) {
+            // res.send(data);
+            // console.log(data);
+            data.forEach((obj) => {
+                db.any(`SELECT group_name, group_id FROM groups where group_id=${obj.group_id}`).
+                    then(function (group) {
+                        console.log(obj, group);
+                        userGroups.push(group[0]);
+                        console.log(userGroups);
                         console.log(counter, data.length)
-                        res.send(JSON.stringify(userGroups));
-                    }
-                });
+                        ++counter;
+                        if (counter == data.length) {
+                            console.log("entered")
+                            console.log(counter, data.length)
+                            res.send(JSON.stringify(userGroups));
+                        }
+                    });
+            })
         })
-    })
 
 });
 
@@ -77,19 +92,19 @@ app.get('/groupmembers/:group_id', (req, res) => {
     console.log(group_id)
     counter = 0;
     db.any(`SELECT user_id from groupmembers where group_id=${group_id}`).
-    then(function(data) {
-        data.forEach((obj) => {
-            db.any(`SELECT user_id, name from users where user_id=${obj.user_id}`).
-            then(function(user) {
-                // console.log(user);
-                groupDetails.push(user[0]);
-                counter++;
-                if(counter == data.length) {
-                    res.send(JSON.stringify(groupDetails))
-                }
+        then(function (data) {
+            data.forEach((obj) => {
+                db.any(`SELECT user_id, name from users where user_id=${obj.user_id}`).
+                    then(function (user) {
+                        // console.log(user);
+                        groupDetails.push(user[0]);
+                        counter++;
+                        if (counter == data.length) {
+                            res.send(JSON.stringify(groupDetails))
+                        }
+                    })
             })
         })
-    })
 
 });
 
@@ -97,16 +112,33 @@ app.post('/signup', (req, res) => {
     console.log(req.body);
     payload = req.body;
     //verify if the req body has both the params and have valid data
-    db.any(`select mailid from users where mailid = ${payload.mailid}`).then(
-        function(data){
+    db.any(`select email_id from users where email_id = '${payload.EmailId}'`).then(
+        function (data) {
             console.log(data)
-            if(data.length == 0) {
-                    //TODO: Implement redis
-                    //storing in local cache using node-cache package
-                    console.log('setting it in the cache')
-                    cache.set(payload?.mailid, payload, 600)
-                    let otp = randomValueGenerator();
-                    res.send({otp: otp})
+            if (data.length == 0) {
+                //TODO: Implement redis
+                //storing in local cache using node-cache package
+                console.log('setting it in the cache')
+                let otp = randomValueGenerator();
+                //send otp to mail id
+                const mailData = {
+                    from: 'vaultwarden@anandu.net',
+                    to: payload?.EmailId,
+                    subject: 'splitwise signup',
+                    text: `Your OTP is ${otp}. Please Enter this number and validate yourself in the application`
+                }
+                // console.log(transportor)
+                transportor.sendMail(mailData, (error, info) => {
+                    if (error) {
+                        res.status(500).send({ 'status': 'internal server error' })
+                        console.log(error)
+                    } else {
+                        res.status(200).send({ 'status': 'otp sent' })
+                        cache.set(payload?.EmailId, { ...payload, "OTP": otp }, 600000)
+                    }
+                })
+            } else {
+                res.status(201).send({ 'status': 'user already exists' })
             }
         }
     )
@@ -117,11 +149,26 @@ app.post('/signup', (req, res) => {
     // res.send("some");
 });
 
-app.post('/verifyotp', (req, res)=> {
-    console.log(req)
-    //query the redis table with the phone number and get the otp
-    //check if it matches with the otp we got
-    //if yes, add the person details to the final users table in the db
+app.post('/validateotp', (req, res) => {
+    // console.log(req.body)
+    let payload = req.body
+    console.log(payload)
+    let cachedObject = cache.get(payload?.EmailId)
+    console.log(cachedObject)
+    if (payload.OTP == cachedObject?.OTP) {
+        console.log("OTP matches. Adding the person to the db")
+        //TODO: remove hardcoded password
+        //TODO: send cookies?
+        db.any('INSERT INTO USERS(email_id, password, name) VALUES($1, $2, $3)', [cachedObject.EmailId, "password", cachedObject.Name]).then(
+            function (data) {
+                console.log(data);
+                res.status(200).send({ "status": "success" });
+            }
+        )
+    } else {
+        res.send({ "status": "failure", "reason": "wrong otp" })
+    }
+    // res.status(300).send('smtng')
 })
 
 app.listen(PORT, () => {
